c367fa50e0ca34ac3fa1a8ecabcf1298c91cb030
imranfix <imran2000hosan@gmail.com>
2022-11-24 20:49:25 +0600

    get category data and secret key hide .env file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
@@ -8,6 +11,45 @@ const port = process.env.PORT || 5000;
 // use middle wares:
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z6welky.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+        const categoryCollection = client.db('oldBook').collection('category');
+
+
+        // 1. get read data form category:
+        app.get('/category', async(req, res) =>{
+            const query = {}
+            const cursor = categoryCollection.find(query);
+            const category = await cursor.toArray();
+            res.send(category);
+        });
+
+        // 2.
+        app.get('/category/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const category = await categoryCollection.findOne(query);
+            res.send(category);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
+
 
 
 
